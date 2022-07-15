00934e6072a6cd45d413abc97f471b9ede62bb89
Sam Pan <pwl12344321@gmail.com>
2022-07-15 21:59:01 +0800

    add filter

--- read2.py
+++ read2.py
@@ -4,8 +4,8 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # count = count + 1
-		if count % 1000 == 0:
-			print(len(data))
+		# if count % 1000 == 0: # can be used
+		# 	print(len(data)) # can be used
 print('Total number of reviews: ', len(data))
 
 sum_len = 0
@@ -13,3 +13,11 @@ for d in data:
 	sum_len += len(d)
 avg = sum_len / len(data)
 print('Average wording is: ', avg)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('the number of reviews with less than 100 words: ', len(new))
+print(new[0])
+print(new[1])
