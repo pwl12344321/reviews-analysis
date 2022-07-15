4f1dd05676799373769ad6338f93dbdd9fdbd318
Sam Pan <pwl12344321@gmail.com>
2022-07-15 21:49:55 +0800

    calc average len of reviews

--- read2.py
+++ read2.py
@@ -3,7 +3,13 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count +=1 # count = count + 1
+		count += 1 # count = count + 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('Total number of reviews: ', len(data))
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+avg = sum_len / len(data)
+print('Average wording is: ', avg)
